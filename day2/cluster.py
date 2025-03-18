7aaaba21a09a1923ed6ba7edbde71fb33723665c
Ashutoshh <ashutoshh@linux.com>
2025-03-18 23:15:52 +0530

    dask updates

--- day2/cluster.py
+++ day2/cluster.py
@@ -1,9 +1,22 @@
 from dask.distributed  import LocalCluster,Client  
+import dask.array as da
+
 # Creating local cluster
 cluster = LocalCluster()
 # Creating client
 client = Client(cluster)
 # Displaying client dashboard
 print(client.dashboard_link)
+# using dask array to create array of 1000 elements
+arr1 = da.ones((1000,1000),chunks=(100,100))
+# performing sum , mean of array elements using dask array
+# these oparation transformation in dask array
+result1 = arr1.sum()
+result2 = arr1.mean()
+# using action call to get the result -- compute
+# this will trigger the computation
+print(result1.compute())
+print(result2.compute())
+
 # shutdown cluster
 client.shutdown()
